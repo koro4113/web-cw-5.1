df265d18c1248dc0605b368d8e5bad9d21eb28a1
koro4113 <66620396+koro4113@users.noreply.github.com>
2020-06-27 20:40:06 +0300

    تمرين جافاسكريبت

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,51 @@
 // السؤال الأول
 // 1
-
+const name = " انا محمد";
 // 2
-
+const counter = 16;
 // 3 and 4
-
+console.log (name + " وعمري " + counter);
 // 5 and 6
-
+console.log (counter * 5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let myhobby = ["الرسم","كرة السله"]
+console.log(myhobby);
 // 2
-
+myhobby.push = (counter);
+console.log(myhobby);
+myhobby.push = ("16");
+console.log(myhobby);
 // 3
-
+myhobby.pop();
+console.log(myhobby);
 // 4
+let chioce = {
+ language: "javescript",
 
+
+};
 // 5
+let ta =["moudhi" , "rahaf" , "Hussien"  ,"nancy"];
 
 // 6
-
+chioce = {
+     language: "javescript",
+      nam: "web",
+      ta: ["hussien"]
+};
 // 7
-
+console.log(ta[2])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
+function callnop(params) {
+    "hello javescript";
+}
 
 // 2
-
-// console results => console.png
+console.log(callnop)
+//console results => console.png
